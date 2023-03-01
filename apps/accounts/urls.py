6c7387851fd4236f8918df2bfacbbf0a12a0fb54
giantolentino <gianpaolotolentino@gmail.com>
2023-02-28 23:38:15 -0800

    Renamed home to homepage.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, LoginView
 
 
 app_name = "accounts"
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", HomePageView.as_view(), name="homepage"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
