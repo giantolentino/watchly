90eabe86c48332f0af7420c4e5efae1c3a87aa90
giantolentino <gianpaolotolentino@gmail.com>
2023-03-01 00:57:23 -0800

    Created RegisterView.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -13,3 +14,9 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     next_page = reverse_lazy("accounts:homepage")
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "accounts/register.html"
