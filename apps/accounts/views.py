8e88c5e087ff5afa6522df83be9ebb32b53f4e3e
giantolentino <gianpaolotolentino@gmail.com>
2023-02-28 23:47:07 -0800

    Made LoginView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 
 class HomePageView(TemplateView):
     template_name = "accounts/homepage.html"
+
+
+class LoginView(LoginView):
+    template_name = "accounts/login.html"
