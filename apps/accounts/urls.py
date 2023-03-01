060ac0f68aa73ab92884dc6747e26eb370a5035c
giantolentino <gianpaolotolentino@gmail.com>
2023-03-01 00:57:02 -0800

    Added route for registration.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, LoginView, LogoutView
+from .views import HomePageView, LoginView, LogoutView, RegisterView
 
 
 app_name = "accounts"
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="homepage"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
