53849fda265b54354889d52880d92969bd5e74c5
giantolentino <gianpaolotolentino@gmail.com>
2023-03-06 10:48:26 -0800

    Added a model  for Bookmark

--- apps/bookmarks/models.py
+++ apps/bookmarks/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    year = models.IntegerField(default=0)
+    posterpath = models.CharField(max_length=255, default="")
+    tmdb_id = models.IntegerField(default=0, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    priority = models.PositiveSmallIntegerField(
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    MEDIA_TYPE_CHOICES = [
+        ("tv", "Tv"),
+        ("movie", "Movie"),
+    ]
+    media_type = models.CharField(
+        max_length=5,
+        choices=MEDIA_TYPE_CHOICES,
+        default="tv",
+    )
+
+    # Each record must have unique combo of following fields.
+    class Meta:
+        unique_together = (("user", "tmdb_id", "media_type"),)
+
+    def __str__(self):
+        return self.title
+
+
+class Tv(Bookmark):
+    pass
+
+
+class Movie(Bookmark):
+    pass
