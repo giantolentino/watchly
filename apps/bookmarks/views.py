0539f876b1abbc8ebf125357d41830f5f80ef0b7
giantolentino <gianpaolotolentino@gmail.com>
2023-03-06 11:17:16 -0800

    Created a listview using Bookmark model.

--- apps/bookmarks/views.py
+++ apps/bookmarks/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Bookmark
 
 
 # Create your views here.
-class BookmarksView(TemplateView):
+class BookmarksView(ListView):
+    model = Bookmark
+    context_object_name = "entries"
     template_name = "bookmarks/bookmarks.html"
