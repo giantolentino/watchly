db6625a2ef9a3abccd3ba023776e8a04c0449ba4
giantolentino <gianpaolotolentino@gmail.com>
2023-03-08 12:07:44 -0800

    Added route for delete_bookmark

--- apps/bookmarks/urls.py
+++ apps/bookmarks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BookmarksView, BookmarkDetailView
+from .views import BookmarksView, BookmarkDetailView, delete_bookmark
 
 
 app_name = "bookmarks"
 urlpatterns = [
     path("", BookmarksView.as_view(), name="index"),
     path("<int:pk>/", BookmarkDetailView.as_view(), name="detailed"),
+    path("delete/<int:pk>", delete_bookmark, name="delete"),
 ]
