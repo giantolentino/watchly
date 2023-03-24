09d0d0f819f732122d0dccb2ebb40241737441d4
giantolentino <gianpaolotolentino@gmail.com>
2023-03-23 20:56:19 -0700

    Chnaged to ListView and added Entry db.

--- apps/history/views.py
+++ apps/history/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Entry
 
 
-class HistoryView(TemplateView):
+class HistoryView(ListView):
+    model = Entry
+    context_object_name = "entries"
     template_name = "history/index.html"
+
+    def get_queryset(self):
+        queryset = Entry.objects.filter(user=self.request.user).order_by("-date_added")
+        return queryset
