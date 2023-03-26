44e0a93c6176faff39697e1093f0480a28b86efe
giantolentino <gianpaolotolentino@gmail.com>
2023-03-25 21:18:09 -0700

    Added route for detailed page.

--- apps/history/urls.py
+++ apps/history/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HistoryView
+from .views import HistoryView, HistoryDetailView
 
 app_name = "history"
 urlpatterns = [
     path("", HistoryView.as_view(), name="index"),
+    path("<int:pk>/", HistoryDetailView.as_view(), name="detailed"),
 ]
