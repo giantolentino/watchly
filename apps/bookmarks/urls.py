effbf05d3a610d060be860fb9bdc4b470c21f4de
giantolentino <gianpaolotolentino@gmail.com>
2023-03-27 16:04:51 -0700

    Added route for moving from bookmarks to history.

--- apps/bookmarks/urls.py
+++ apps/bookmarks/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import BookmarksView, BookmarkDetailView, delete_bookmark
+from .views import (
+    BookmarksView,
+    BookmarkDetailView,
+    delete_bookmark,
+    move_bookmark_to_history,
+)
 
 
 app_name = "bookmarks"
@@ -7,4 +12,5 @@ urlpatterns = [
     path("", BookmarksView.as_view(), name="index"),
     path("<int:pk>/", BookmarkDetailView.as_view(), name="detailed"),
     path("delete/<int:pk>", delete_bookmark, name="delete"),
+    path("move/<int:pk>", move_bookmark_to_history, name="move"),
 ]
