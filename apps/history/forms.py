2480c9659edd8e0d57c1e78a42cf570db88af0df
giantolentino <gianpaolotolentino@gmail.com>
2023-04-16 23:25:47 -0700

    auto fills todays date for date_watched

--- apps/history/forms.py
+++ apps/history/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Entry
+from datetime import date
 
 
 class HistoryEntryForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(HistoryEntryForm, self).__init__(*args, **kwargs)
+        today = date.today().strftime("%Y-%m-%d")
+        self.fields["date_watched"].initial = today
+
     class Meta:
         model = Entry
         fields = [
@@ -25,7 +31,7 @@ class HistoryEntryForm(ModelForm):
             "media_type": forms.HiddenInput(),
             "description": forms.HiddenInput(),
             "rating": forms.NumberInput(attrs={"class": "form-control"}),
-            "date_watched": forms.DateInput(attrs={"type": "date"}),
+            "date_watched": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d"),
             "feedback": forms.Textarea(
                 attrs={"class": "form-control", "rows": 4, "required": False}
             ),
